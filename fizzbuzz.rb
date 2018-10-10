8a90b9e70393be815e770e30a7a21b296e887c7c
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-10 20:32:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz (argument)
-  if argument % 3 == 0
+  if (argument % 3 == 0)
     puts "Fizz"
-    if argument % 5 == 0
+    elsif (argument % 5 == 0)
       puts "Buzz"
-     if argument % 3 && 5 == 0
+    else (argument % 3 && 5 == 0)
        puts "FizzBuzz" 
 end
 end
