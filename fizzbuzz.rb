25f0d9d2e63e136235a61170c19c55dd51921bf2
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-10 20:45:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
   return "Fizz"
+  
   elsif int % 5 == 0
-   return puts "Buzz"
+  return "Buzz"
+  
   elsif int % 3 == 0 && int % 5 == 0
    return puts "FizzBuzz"
 
